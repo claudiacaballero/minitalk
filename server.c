dde551faf5068f5d5371fdb06306107903bad0a1
Claudia Caballero Coll <ccaballe@car13s1.42barcelona.com>
2023-02-08 18:59:48 +0100

    all

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: ccaballe <ccaballe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 12:50:18 by ccaballe          #+#    #+#             */
-/*   Updated: 2023/02/08 12:52:55 by ccaballe         ###   ########.fr       */
+/*   Updated: 2023/02/08 18:15:17 by ccaballe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,27 @@ int	main(void)
 	pid = getpid();
 	if (ft_printf("%i\n", pid) == -1)
 		exit(1);
-	recieve_signal();
+	signal(SIGUSR1, &get_byte(SIGUSR1));
+	signal(SIGUSR2, &get_byte(SIGUSR2));
 	while (1)
 		pause();
 	return (0);
 }
 
-void	receive_signal(void)
+void	get_byte(int sig)
 {
-	char	byte[8];
+	int	bits[8];
+	int	i;
 
-	sigaction()
+	i = 0;
+	if (sig == SIGUSR1)
+		bits[i] = 1;
+	else
+		bits[i] = 0;
+	i++;
 }
 
-/*void	byte_to_char(int sig)
+/*void	byte_to_char(int *bits)
 {
 	int	base;
 	int	c;
