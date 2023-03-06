f4f8e6c00cd3d64a3c0c60583c287b8d094b6a26
Claudia Caballero Coll <ccaballe@car10s6.42barcelona.com>
2023-03-06 15:58:10 +0100

    bonus mostly done

--- bonus/minitalk_bonus.h
+++ bonus/minitalk_bonus.h
@@ -6,7 +6,7 @@
 /*   By: ccaballe <ccaballe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 13:02:52 by ccaballe          #+#    #+#             */
-/*   Updated: 2023/03/06 15:31:58 by ccaballe         ###   ########.fr       */
+/*   Updated: 2023/03/06 15:51:57 by ccaballe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@
 # include "../libft/libft.h"
 # include "../ft_printf/ft_printf.h"
 
-void	get_byte(int sig, pid_t pid);
 int		valid_pid(char *s);
 void	char_to_byte(char *s, pid_t pid);
 void	byte_to_char(int *bits, pid_t pid);
-void	handler(int signo, siginfo_t *info, void *other);
+void	handler_server(int signo, siginfo_t *info, void *other);
 void	handler_client(int signo, siginfo_t *info, void *other);
 
 #endif
--- bonus/server_bonus.c
+++ bonus/server_bonus.c
@@ -6,16 +6,11 @@
 /*   By: ccaballe <ccaballe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 13:04:32 by ccaballe          #+#    #+#             */
-/*   Updated: 2023/03/06 15:16:40 by ccaballe         ###   ########.fr       */
+/*   Updated: 2023/03/06 15:57:38 by ccaballe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
-
-// server envia sigusr1
-// quan rebi '\0' envia un sigusr2
-// quan el client rebi sigusr2 exit(0)
-// el client fa un printf amb \r dels bits enviats
 
 int	main(void)
 {
@@ -25,7 +20,7 @@ int	main(void)
 	pid_server = getpid();
 	if (ft_printf("%i\n", pid_server) == -1)
 		exit(1);
-	sa.sa_sigaction = &handler;
+	sa.sa_sigaction = &handler_server;
 	sa.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		exit(1);
@@ -36,27 +31,22 @@ int	main(void)
 	return (0);
 }
 
-void	handler(int signo, siginfo_t *info, void *other)
-{
-	(void)other;
-	get_byte(signo, info->si_pid);
-}
-
-void	get_byte(int sig, pid_t pid_client)
+void	handler_server(int signo, siginfo_t *info, void *other)
 {
 	static int	bits[8];
 	static int	i = 0;
 
-	if (kill(pid_client, SIGUSR1) == -1)
+	(void)other;
+	if (kill(info->si_pid, SIGUSR1) == -1)
 		exit(1);
-	if (sig == SIGUSR1)
+	if (signo == SIGUSR1)
 		bits[i] = 1;
 	else
 		bits[i] = 0;
 	i++;
 	if (i == 8)
 	{
-		byte_to_char(bits, pid_client);
+		byte_to_char(bits, info->si_pid);
 		i = 0;
 	}
 }
